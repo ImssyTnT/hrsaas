ee00b2d7d0b523d3955ef4921bcad36e975a543b
ImssyTnT <562285429@qq.com>
2022-08-02 12:17:04 +0800

    完成了主页的登录权限

---
+++ src/permission.js
@@ -0,0 +1,25 @@
+import router from '@/router'
+import store from '@/store'
+
+// 全局路由前置守卫
+// 会在全局路由进入之前触发
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+  const token = store.state.user.token
+  if (token) {
+    // 1.登录
+    // 判断是否去往登录页面
+    // 如果是 强制跳转到主页
+    if (to.path === '/login') return next('/')
+    // 如果不是 放行
+    next()
+  } else {
+    // 2.未登录
+    // 判断去往的页面是否在白名单内
+    const isCludes = whiteList.includes(to.path)
+    // 如果在 放行
+    if (isCludes) return next()
+    // 如果不在 强制跳转到登录页面
+    next('/login')
+  }
+})
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -47,7 +47,7 @@
       >
 
       <div class="tips">
-        <span style="margin-right: 20px">账号: 13800000002</span>
+        <span style="margin-right: 20px">账号: 13800000004</span>
         <span> 密码: 123456</span>
       </div>
     </el-form>
@@ -61,7 +61,7 @@ export default {
     return {
       // 表单输入框双向绑定数据
       loginForm: {
-        mobile: '13800000002',
+        mobile: '13800000004',
         password: '123456',
       },
       // 表单校验规则
